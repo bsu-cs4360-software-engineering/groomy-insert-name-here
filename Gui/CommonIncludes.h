3f9837daa08e6f27517b088670810a16a7b9bce0
Michael W. <mman777mw@yahoo.com>
2024-11-20 22:34:23 -0600

    Updating Userinfo
    Now with hashMe

--- Gui/CommonIncludes.h
+++ Gui/CommonIncludes.h
@@ -17,3 +17,18 @@
 #include <filesystem>
 
 using namespace std;
+using json = nlohmann::json;
+
+
+string hashMe(const string& input, int modulus) {
+    string result;
+    string pepperedInput = input + 'M';
+
+    for (char c : pepperedInput) {
+        int asciiValue = static_cast<int>(c); // Convert character to ASCII
+        int modValue = asciiValue % modulus; // Apply the modulo operation
+        result += to_string(modValue);       // Append the result as a string
+    }
+
+    return result;
+}
--- Gui/UserInfo.h
+++ Gui/UserInfo.h
@@ -3,34 +3,89 @@
 
 namespace fs = std::filesystem;
 
+namespace fs = std::filesystem;
+
 class UserInfo
 {
 private:
-	string username;
-	string password;
+    string username;
+    string password;
     string email;
     string first_name;
     string last_name;
     string user_file;
 
 public:
-	string usernameEntry;
-	string passwordEntry;
+    string usernameEntry;
+    string passwordEntry;
     UserInfo(const string& username, const string& password, const string& email = "", const string& first_name = "", const string& last_name = ""):
         username(username), password(password), email(email), first_name(first_name), last_name(last_name)
     {
 
     }
-	bool login() 
-	{
-		if (usernameEntry == username && passwordEntry == password)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+    static bool login(const string& username, const string& password) {
+        string user_data_file = username + ".json";
+
+        if (!fs::exists(user_data_file)) {
+            cout << "Username not found." << endl;
+            return false;
+        }
+
+
+        ifstream file(user_data_file);
+        json user_data;
+        file >> user_data;
+        file.close();
+
+        string stored_password = user_data["password"];
+
+        if (hashMe(password, 11) != stored_password)
+        {
+            cout << "Error! Username or Password invalid.\nPlease verify correct username and password before trying again.";
+            return false;
+        }
+        else if (hashMe(password, 11) == stored_password)
+        {
+            cout << "Login Successful.";
+            return true;
+        }
+        else {
+            cout << "Unkown error occured. Please retry.";
+            return false;
+        }
+
+
+
+
+    }
+    bool register_User()
+    {
+        if(fs::exists(user_file))
+        {
+            cout << "User already exists." << endl;
+            return false;
+        }
+
+        string hashed_pass = hashMe(password, 11);
+
+        json user_data = {{"username", username},
+            {"password", hashed_pass},
+            {"email", email},
+            {"first_name", first_name},
+            {"last_name", last_name}
+        };
+
+        ofstream file(user_file);
+        file << user_data.dump(4);
+        file.close();
+
+        cout << "User created successfully." << endl;
+        return true;
+
+    }
+    static bool create_account(const string& username, const string& password, const string& email, const string& first_name, const string& last_name) {
+        UserInfo user(username, password, email, first_name, last_name);
+        return user.register_User();
+    }
 
 };
